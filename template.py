0948af2b0f193cd19787fefe04483b1b5e04c3d5
sanjanakokkonda <122037420+sanjanakokkonda@users.noreply.github.com>
2024-12-19 15:27:37 +0530

    requirements added

--- template.py
+++ template.py
@@ -22,6 +22,7 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files: #this loop will create path so that it doesnt throw any error
